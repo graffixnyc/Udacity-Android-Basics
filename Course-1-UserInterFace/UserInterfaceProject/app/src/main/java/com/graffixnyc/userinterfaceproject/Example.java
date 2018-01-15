1ea06ab84cd253264ea26380691dbbf289403a26
Patrick Hill <graffixnyc@gmail.com>
2018-01-14 20:51:58 -0500

    JustJava App

---
+++ Course-1-UserInterFace/UserInterfaceProject/app/src/main/java/com/graffixnyc/userinterfaceproject/Example.java
@@ -0,0 +1,12 @@
+package com.graffixnyc.userinterfaceproject;
+
+/**
+ * Created by graffixnyc on 1/14/18.
+ */
+
+public class Example {
+    private String mFirstName;
+}
+
+
+
--- Course-1-UserInterFace/UserInterfaceProject/app/src/main/java/com/graffixnyc/userinterfaceproject/MainActivity.java
+++ Course-1-UserInterFace/UserInterfaceProject/app/src/main/java/com/graffixnyc/userinterfaceproject/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Example myex = new Example();
     }
 }
